0e9eabbca9d29f2dadfe7ad2e093861418abae46
THIMOTHEE Rudy <dathims@gmail.com>
2017-07-12 01:45:55 +0200

    build before deploying

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -17,26 +17,24 @@ angular.module('barista')
 
     vm.config = {
 				sources: [
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-					{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-				]}
+					{src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'},
+					{src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.webm'), type: 'video/webm'},
+					{src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.ogg'), type: 'video/ogg'}
+				]};
 
     drive.getRessourcesItem(urlId)
       .then(function(result) {
         var res = result.data.entry;
-        vm.product.title = res.gsx$title.$t,
-        vm.product.img = res.gsx$img.$t,
-        vm.product.type = res.gsx$type.$t,
-        vm.product.director = res.gsx$director.$t,
-        vm.product.description = res.gsx$description.$t,
-        vm.product.trailer = res.gsx$trailer.$t,
-        vm.product.link = res.gsx$link.$t,
-        vm.product.category = res.gsx$category.$t,
-        vm.product.episode = res.gsx$episode.$t
+        vm.product.title = res.gsx$title.$t;
+        vm.product.img = res.gsx$img.$t;
+        vm.product.type = res.gsx$type.$t;
+        vm.product.director = res.gsx$director.$t;
+        vm.product.description = res.gsx$description.$t;
+        vm.product.trailer = res.gsx$trailer.$t;
+        vm.product.link = res.gsx$link.$t;
+        vm.product.category = res.gsx$category.$t;
+        vm.product.episode = res.gsx$episode.$t;
       }, function(error) {
         console.error(error);
       });
-
-      
 }]);
